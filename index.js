b95d3342a910fc24fe2c8ee55d612189127e5f5f
Tomas Perez <tomasitopg6@gmail.com>
2021-06-02 05:34:43 +0000

    progress

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let formData = {
+  name: "Steve",
+  email: "steve@steve.com"
+};
+
+function submitData() = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData);
+
+  fetch("http://localhost:3000/users", submitData)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+};
