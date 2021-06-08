825b968dd5a06db5a11d846e2492aff42e5d5a30
Tomas Perez <tomasitopg6@gmail.com>
2021-06-08 20:42:13 +0000

    done

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 // Add your code here
-let formData = {
-  name: "Steve",
-  email: "steve@steve.com"
-};
-
-  const formInfo = { 
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    }, 
-    body: JSON.stringify(formData)};
- 
- 
-
-  function submitData() {
-  return fetch("http://localhost:3000/users", formInfo)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(object) {
-      console.log(object);
-    })
-    .catch(function(error) {
-      alert("Bad things! Ragnarők!");
-      console.log(error.message);
-    });
-  }
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+}
